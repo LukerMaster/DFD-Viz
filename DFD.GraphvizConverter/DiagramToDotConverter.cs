f6bcc4176357bbd77b5b399d58f0c18878d035a8
LukerMaster <lukermaster@proton.me>
2023-11-15 20:13:36 +0100

    Added visual representations of Processes with subprocesses inside

--- DFD.GraphvizConverter/DiagramToDotConverter.cs
+++ DFD.GraphvizConverter/DiagramToDotConverter.cs
@@ -3,27 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DFD.Model;
 using DFD.Model.Interfaces;
 
 namespace DFD.GraphvizConverter
 {
     public class DiagramToDotConverter
     {
+        private IGraphEntity GetRoot(IDiagram diagram)
+        {
+            IGraphEntity Root = diagram.Entities.First();
+            while (Root.Parent is not null)
+            {
+                Root = Root.Parent;
+            }
+            return Root;
+        }
+
+        private string RepresentNode(IGraphEntity node, string code)
+        {
+            if (node.Children.Count == 0) // Draw only leaves
+                code += $"{node.FullEntityName.Replace('.', '_')}; \n";
+            else
+            {
+                foreach (var child in node.Children)
+                {
+                    code += $"subgraph {child.FullEntityName.Replace('.', '_')} \n" +
+                            $"{{ label={child.FullEntityName.Replace('.','_')} \n " +
+                            $"cluster=True \n";
+                    code = RepresentNode(child, code);
+                    code += "} \n";
+                }
+            }
+            return code;
+        }
+
         public string ToDot(IDiagram diagram)
         {
             string code = "digraph { ";
-            foreach (var node in diagram.Entities)
-            {
-                if (node.Children.Count == 0) // Draw only leaves
-                    code += $"{node.FullEntityName.Replace('.', '_')}; \n";
-            }
-
+            code = RepresentNode(GetRoot(diagram), code);
             foreach (var flow in diagram.Flows)
             {
                 code += $"{flow.Source.FullEntityName.Replace('.', '_')} -> {flow.Target.FullEntityName.Replace('.', '_')}; \n";
             }
 
-            code += " }";
+            code += " } \n";
             return code;
         }
     }
--- DFD.Model/Interfaces/IGraphEntity.cs
+++ DFD.Model/Interfaces/IGraphEntity.cs
@@ -4,7 +4,7 @@ namespace DFD.Model.Interfaces;
 
 public interface IGraphEntity
 {
-    IGraphEntity Parent { get; set; }
+    IGraphEntity? Parent { get; set; }
     ICollection<IGraphEntity> Children { get; set; }
     public string EntityName { get; }
     public string FullEntityName
