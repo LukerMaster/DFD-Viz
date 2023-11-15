980a605f621f0e4d1536b7f0541473040a59e036
LukerMaster <lukermaster@proton.me>
2023-11-15 18:14:05 +0100

    Moved exceptions to seperate file

---
+++ DFD.Model/Exceptions.cs
@@ -0,0 +1,20 @@
+﻿using DFD.Model.Interfaces;
+
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException(string message) : base(message)
+    {
+
+    }
+}
+
+public class AmbiguousEntityMatchException : Exception
+{
+    public string EntityName { get; }
+    public IGraphEntity[] Candidates { get; }
+    public AmbiguousEntityMatchException(string entityName, params IGraphEntity[] candidates)
+    {
+        EntityName = entityName;
+        Candidates = candidates;
+    }
+}
--- DFD.Model/Interfaces/IGraphEntity.cs
+++ DFD.Model/Interfaces/IGraphEntity.cs
@@ -91,21 +91,3 @@ public interface IGraphEntity
     }
 }
 
-public class EntityNotFoundException : Exception
-{
-    public EntityNotFoundException(string message) : base(message)
-    {
-        
-    }
-}
-
-public class AmbiguousEntityMatchException : Exception
-{
-    public string EntityName { get; }
-    public IGraphEntity[] Candidates { get; }
-    public AmbiguousEntityMatchException(string entityName, params IGraphEntity[] candidates)
-    {
-        EntityName = entityName;
-        Candidates = candidates;
-    }
-}
