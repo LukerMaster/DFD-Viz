00b4ce62a8832aca3932ceb67c32849350ed6aef
LukerMaster <lukermaster@proton.me>
2023-11-22 00:00:13 +0100

    Switched Graphviz runner to in-memory mode

--- DFD.GraphvizConverter/GraphvizRunner.cs
+++ DFD.GraphvizConverter/GraphvizRunner.cs
@@ -2,7 +2,7 @@
 
 public class GraphvizRunner
 {
-    private byte[] GetGraph(string dotCode, string outputType)
+    private byte[] GetGraphWithTempFiles(string dotCode, string outputType)
     {
         string fileName = "temp-output-graphviz-" + Guid.NewGuid().ToString() + "." + outputType;
         string tempFilePath = Path.Combine(Path.GetTempPath(), fileName);
@@ -11,14 +11,20 @@ public class GraphvizRunner
         byte[] fileOutput = File.ReadAllBytes(tempFilePath);
         return fileOutput;
     }
+
+    private byte[] GetGraphInMemory(string dotCode, string outputType)
+    {
+        var runner = new GraphVizNet.GraphViz();
+        return runner.LayoutAndRender(null, dotCode, null, null, outputType);
+    }
     public string GetGraphAsJson(string dotCode)
     {
-        return System.Text.Encoding.UTF8.GetString(GetGraph(dotCode, "json"));
+        return System.Text.Encoding.UTF8.GetString(GetGraphInMemory(dotCode, "json"));
     }
 
     public byte[] GetGraphAsPng(string dotCode)
     {
-        return GetGraph(dotCode, "png");
+        return GetGraphInMemory(dotCode, "png");
     }
 
     public void OutputDebugGraphAsPng(string dotCode, string outputPath = "./debug.png")
