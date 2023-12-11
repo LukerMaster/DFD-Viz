aa7ebf9be85847a2907da643ccc9910047b3ce58
LukerMaster <lukermaster@proton.me>
2023-12-11 15:14:53 +0100

    Added AvaloniaUI editor project.

---
+++ DFD.AvaloniaEditor/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DFD.ViewModel.Interfaces;
+
+namespace DFD.AvaloniaEditor.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public IVisualGraph GraphViewModel;
+    public string Greeting => "Welcome to Avalonia!";
+    public string TestString => "";
+}
---
+++ DFD.AvaloniaEditor/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DFD.AvaloniaEditor.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
